46e5c89eb3b3282effa82c68c6e618baa96d9113
Amir Salehi <nik.amirsa@gmail.com>
2022-07-07 11:17:29 +0430

    update details

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Snowa store chains</h1>
+  <p>I just use vite for launch dev server</p>
 `
